7304cc1f1ef0831f7f6c04be1a0aff77adaae9c0
Matthew Wilkinson <matt@somerobots.com>
2019-04-15 11:14:13 +0800

    Initial Commit

---
+++ SalesTaxCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SalesTaxCalc
+//
+//  Created by Matthew Wilkinson on 15/4/19.
+//  Copyright © 2019 Some Robots. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
